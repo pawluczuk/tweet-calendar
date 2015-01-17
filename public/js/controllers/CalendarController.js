737e1b543eb97ecb34cb9a89c8259b6e79545c94
unknown <mmalesa92@gmail.com>
2015-01-18 00:13:35 +0100

    kurwa mac : D

--- public/js/controllers/CalendarController.js
+++ public/js/controllers/CalendarController.js
@@ -1,5 +1,21 @@
 angular.module('tweetCalendarApp.controllers').controller('CalendarCtrl', function($http, $scope, CalendarService){
-	$scope.project = {};
+	$scope.event = {};
 
-	console.log("JP2GMD");
+	socket.on('event-created', function(data) {
+		var response = 'Utworzyles nowe wydarzenie : ' + data.response;
+		console.log(response);
+		$('#calendar').fullCalendar('refetchEvents');
+	});
+
+	$scope.saveEvent = function()
+	{
+		$scope.event.start = $('#eventStart').val();
+		$scope.event.end = $('#eventEnd').val();
+		$scope.event.ownerId = $('#ownerId').val();
+
+		socket.emit('create-event', $scope.event);
+
+		$('#Modalerino').modal('hide');
+		$scope.event = {};
+	}
 });
