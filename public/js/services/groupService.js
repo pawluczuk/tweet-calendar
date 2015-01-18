9def9578ab30e3e17509dca42c2628f22cf2140d
unknown <mmalesa92@gmail.com>
2015-01-18 19:27:52 +0100

    groupController, groupService bodies

--- public/js/services/groupService.js
+++ public/js/services/groupService.js
@@ -1,5 +1,17 @@
 angular.module('tweetCalendarApp.services').factory('GroupService', function($http) {
 	var factory = {};
 
+	factory.getGroupsOwner = function() {
+		return $http.get("/resources/groupsOwner").then(function(result) {
+			return result.data;
+		});
+	};
+
+	factory.getGroupUsers = function(groupID) {
+		return $http.get("/resources/groupUsers?groupID=" + groupID).then(function(result) {
+			return result.data;
+		});
+	};
+
 	return factory;
 });
