bc955f3694705d564ffbf2b3c952ac5df264b4fa
lem <blemiec@gmail.com>
2015-01-18 17:47:55 +0100

    select groups for event

--- public/js/controllers/eventController.js
+++ public/js/controllers/eventController.js
@@ -1,11 +1,19 @@
 angular.module('tweetCalendarApp.controllers').controller('EventCtrl', function($http, $scope, EventService){
     $scope.eventInfo = { };
     $scope.users = { };
+    $scope.groups = { };
 
     $scope.newUser;
+    $scope.newGroup;
 
     socket.on('users-added', function(data) {
         var response = 'Dodales uzytkownikow do wydarzenia : ' + data.response;
+        console.log(response);
+    });
+
+
+    socket.on('groups-added', function(data) {
+        var response = 'Dodales grupy do wydarzenia : ' + data.response;
         console.log(response);
     });
 
@@ -14,6 +22,13 @@ angular.module('tweetCalendarApp.controllers').controller('EventCtrl', function(
         var data = { 'eventID' : $scope.eventInfo.event_id, 'users' : [ userEmail ]};
         console.log(data);
         socket.emit('add-users', data);
+    }
+
+    $scope.addGroup = function(groupName)
+    {
+        var data = { 'eventID' : $scope.eventInfo.event_id, 'groups' : [ groupName ]};
+        console.log(data);
+        socket.emit('add-groups', data);
     }
 
     $scope.deleteUser = function(userID)
@@ -30,5 +45,9 @@ angular.module('tweetCalendarApp.controllers').controller('EventCtrl', function(
         EventService.getUsers(eventID).then(function(data) {
             $scope.users = data;
         });
+
+        EventService.getGroups(eventID).then(function(data) {
+            $scope.groups = data;
+        })
     }
 });
