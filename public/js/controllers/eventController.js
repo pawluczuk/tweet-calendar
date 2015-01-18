c9c1c072603b4472180dcd2c2ab02f1cd0d74e6d
unknown <mmalesa92@gmail.com>
2015-01-18 17:33:09 +0100

    front-end dodawanie/usuwanie userow z eventu

--- public/js/controllers/eventController.js
+++ public/js/controllers/eventController.js
@@ -4,31 +4,49 @@ angular.module('tweetCalendarApp.controllers').controller('EventCtrl', function(
 
     $scope.newUser;
 
-    socket.on('users-added', function(data) {
+    socket.on('emails-added', function(data) {
         var response = 'Dodales uzytkownikow do wydarzenia : ' + data.response;
         console.log(response);
+        $scope.getUsers($scope.eventInfo.event_id);
+    });
+
+    socket.on('users-deleted', function(data) {
+        var response = 'Usunales uzytkownikow z wydarzenia : ' + data.response;
+        console.log(response);
+        $scope.getUsers($scope.eventInfo.event_id);
     });
 
     $scope.addUser = function(userEmail)
     {
         var data = { 'eventID' : $scope.eventInfo.event_id, 'users' : [ userEmail ]};
-        console.log(data);
-        socket.emit('add-users', data);
+        socket.emit('add-emails', data);
     }
 
     $scope.deleteUser = function(userID)
     {
-        console.log(userID);
+        var data = { 'eventID' : $scope.eventInfo.event_id, 'users' : [ userID ]}
+        socket.emit('delete-users', data)
     }
 
     $scope.init = function(eventID)
     {
+        $scope.getEventInfo(eventID);
+
+        $scope.getUsers(eventID);
+    }
+
+    $scope.getEventInfo = function(eventID)
+    {
         EventService.getEventInfo(eventID).then(function(data) {
             $scope.eventInfo = data[0];
         });
+    }
 
+    $scope.getUsers = function(eventID)
+    {
         EventService.getUsers(eventID).then(function(data) {
             $scope.users = data;
         });
     }
+
 });
